ff88a5a57f143b91eb067cee7cb411a7a9a81cd3
jakezweifler <jake.zweifler@gmail.com>
2019-03-20 12:09:58 -0400

    Innitial commit

--- src/main/java/JavaIOTester.java
+++ src/main/java/JavaIOTester.java
@@ -14,7 +14,7 @@ public class JavaIOTester {
 
         String someData = "The capital of New York is Albany";
         String fileName = "NewYorkCapital";
-        Path path = Paths.get("/home/jake/Tutoring/src/Students/" + fileName);
+        Path path = Paths.get("/home/jake/IdeaProjects/Tutoring/src/main/java/Students/");
 
         if (Files.notExists(path)) {
             try {
@@ -24,11 +24,11 @@ public class JavaIOTester {
             }
         }
 
-        try {
-            Files.writeString(path, someData);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Files.writeString(path, someData);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
